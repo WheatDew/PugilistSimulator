0129b6c0f5aff6fd1aea3ea5a5a149f327a54b25
WheatDew <57761164+WheatDew@users.noreply.github.com>
2020-12-25 18:42:23 +0800

    创建项目

---
+++ Assets/Scripts/AnimatorManagement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatorManagement : MonoBehaviour
+{
+    public Animator ani;
+    public Text self, opponent;
+    private float timer,targetTime;
+    
+
+    public void SetAction(string actName)
+    {
+        ani.SetTrigger(actName);
+        self.text = actName;
+        targetTime = 3;
+    }
+
+    private void Update()
+    {
+        timer+=Time.deltaTime;
+        if(timer>targetTime)
+        {
+            self.text = "";
+            timer = 0;
+        }
+    }
+}
